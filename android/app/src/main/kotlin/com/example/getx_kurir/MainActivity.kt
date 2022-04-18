0567c392a5bffa92db61f99976096a7bd1536a3b
Muhammad Gifari <muhgifari00@gmail.com>
2022-04-19 02:24:08 +0800

    New Project Onkgos Kirim

---
+++ android/app/src/main/kotlin/com/example/getx_kurir/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getx_kurir
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomeView'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        controller.increment();
+      }),
+      body: Obx(
+        () => Center(
+          child: Text(
+            '${controller.count}',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
